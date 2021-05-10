fc922a29e26e4fe3ba3ef92ba8ca05042e69497e
theri <lukasz.kryscio@gmail.com>
2021-05-10 15:06:05 +0200

    Added: -product mass window -calculating carbs, proteins, fats and kcal -reset program button

--- GUI.py
+++ GUI.py
@@ -2,14 +2,20 @@
 
 from tkinter import *
 import pandas as pd
+import os
 
 # Products database
 df = pd.read_csv("calorie_base.csv")
 
-# Counter for adequate row
-counter = 0
+# Global variables
+counter = 2             # Counter for adequate row
+weight = 0
+carbs = 0
+proteins = 0
+fats = 0
+calories = 0
 
-# Window object
+# Main window object
 window = Tk()
 
 # Define entries
@@ -42,35 +48,56 @@ l6.grid(row=99, column=0)
 l7 = Label(window, text="Amount (grams)", padx=20, pady=20)
 l7.grid(row=0, column=5)
 
-l8 = Label(window, text="Delete buttons", padx=20, pady=20)
-l8.grid(row=0, column=6)
 
 # Add product by pressing "add button"
 def add_product_from_list():
-    global counter
+    global counter, carbs, proteins, fats, calories
     counter += 1
     add_listed_product = name_entry.get()
 
-    weight_entry = Entry(window, width=15)
-    weight_entry.grid(row=counter, column=5)
+    carb = round(df.loc[df['Name'] == add_listed_product, 'Carbs'].item()*weight/100, 3)
+    protein = round(df.loc[df['Name'] == add_listed_product, 'Protein'].item()*weight/100, 3)
+    fat = round(df.loc[df['Name'] == add_listed_product, 'Fat'].item()*weight/100, 3)
+    kcal = round(df.loc[df['Name'] == add_listed_product, 'Calories'].item()*weight/100, 3)
 
-    l1 = Label(window, text=df.loc[df['Name']==add_listed_product,'Name'].item(), padx=20, pady=5)
-    l1.grid(row=counter, column=0)
+    carbs = round(carbs + carb, 3)
+    proteins = round(proteins + protein, 3)
+    fats = round(fats + fat, 3)
+    calories = round(calories + kcal, 3)
 
-    l2 = Label(window, text=df.loc[df['Name']==add_listed_product,'Carbs'].item(), padx=20, pady=5)
-    l2.grid(row=counter, column=1)
+    label1 = Label(window, text=df.loc[df['Name'] == add_listed_product, 'Name'].item(), padx=20, pady=5)
+    label1.grid(row=counter, column=0)
 
-    l3 = Label(window, text=df.loc[df['Name']==add_listed_product,'Protein'].item(), padx=20, pady=5)
-    l3.grid(row=counter, column=2)
+    label2 = Label(window, text=carb, padx=20, pady=5)
+    label2.grid(row=counter, column=1)
 
-    l4 = Label(window, text=df.loc[df['Name']==add_listed_product,'Fat'].item(), padx=20, pady=5)
-    l4.grid(row=counter, column=3)
+    label3 = Label(window, text=protein, padx=20, pady=5)
+    label3.grid(row=counter, column=2)
 
-    l5 = Label(window, text=df.loc[df['Name']==add_listed_product,'Calories'].item(), padx=20, pady=5)
-    l5.grid(row=counter, column=4)
+    label4 = Label(window, text=fat, padx=20, pady=5)
+    label4.grid(row=counter, column=3)
+
+    label5 = Label(window, text=kcal, padx=20, pady=5)
+    label5.grid(row=counter, column=4)
+
+    label6 = Label(window, text=weight, padx=20, pady=5)
+    label6.grid(row=counter, column=5)
+
+    l8 = Label(window, text=carbs, padx=20, pady=20)
+    l8.grid(row=99, column=1)
+
+    l9 = Label(window, text=proteins, padx=20, pady=20)
+    l9.grid(row=99, column=2)
+
+    l10 = Label(window, text=fats, padx=20, pady=20)
+    l10.grid(row=99, column=3)
+
+    l11 = Label(window, text=calories, padx=20, pady=20)
+    l11.grid(row=99, column=4)
 
     print(df.loc[df['Name'] == add_listed_product])
     print(counter)
+
 
 # Update the listbox
 def update(data):
@@ -78,10 +105,12 @@ def update(data):
     for product in data:
         product_list.insert(END, product)
 
+
 # Update entry box with listbox clicked
 def fillout(event):
     name_entry.delete(0, END)                   # Clear entry box
-    name_entry.insert(0, product_list.get(ACTIVE))          #Add clicked list item to entry box
+    name_entry.insert(0, product_list.get(ACTIVE))          # Add clicked list item to entry box
+
 
 # Create function to check whether product is on the list
 def check(event):
@@ -95,9 +124,36 @@ def check(event):
                 data.append(item)
     update(data)        # Update listbox with selected product
 
+
+# Product mass input window
+def mass():
+
+    def complete_product():
+        global weight
+        weight = int(mass_entry.get())
+        top.destroy()
+        add_product_from_list()
+
+    top = Toplevel()
+    top.title('Type product mass')
+    mass_entry = Entry(top, width=25)
+    mass_entry.pack()
+    add_button = Button(top, text='ADD', padx=10, pady=10, command=complete_product)
+    add_button.pack()
+
+
+# Reset calculator function
+def restart():
+    window.destroy()
+    os.startfile("GUI.py")
+
+
 # Define buttons
-add_product = Button(window, text="Add product", padx=20, pady=20, command=add_product_from_list)
+add_product = Button(window, text="Add product", padx=20, pady=20, command=mass)
 add_product.grid(row=0, column=7)
+
+reset_button = Button(window, text="RESET", padx=20, pady=20, command=restart)
+reset_button.grid(row=0, column=8)
 
 # Update function call
 update(df['Name'])
@@ -105,7 +161,7 @@ update(df['Name'])
 # Create a binding on the listbox onclick
 product_list.bind("<<ListboxSelect>>", fillout)
 
-#Create a binding on the entry box
+# Create a binding on the entry box
 name_entry.bind("<KeyRelease>", check)
 
 window.mainloop()
